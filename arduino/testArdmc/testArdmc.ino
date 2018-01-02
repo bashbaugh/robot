67fd7c992de7f775f7dfa01c356c6dc495d500a6
Benjamin <ashbau.benjam19@svvsd.org>
2018-01-02 12:44:19 -0700

    added testArdmc

---
+++ arduino/testArdmc/testArdmc.ino
@@ -0,0 +1,25 @@
+//tests the motor control circuit-- drives forward, then side to side, then backward
+const int tleft = 8; 
+const int tright = 7;
+const int forward = 11;
+const int backward = 10;
+
+
+void loop() {
+  delay(4000);
+  digitalWrite(forward, HIGH);
+  delay(1000);
+  digitalWrite(forward, LOW);
+  delay(1000);
+  digitalWrite(tleft, HIGH);
+  delay(1000);
+  digitalWrite(tleft, LOW);
+  digitalWrite(tright, HIGH);
+  delay(1000);
+  digitalWrite(tright, LOW);
+  delay(2000);
+  digitalWrite(backward, HIGH);
+  delay(500);
+  digitalWrite(backward, LOW);
+  delay(5000);
+}
