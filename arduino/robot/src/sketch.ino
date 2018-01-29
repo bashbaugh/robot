fb0045a04db294073be5e30e44dca1e0be5077df
Benjamin <ashbau.benjam19@svvsd.org>
2018-01-28 19:25:58 -0700

    v1.0

--- arduino/robot/src/sketch.ino
+++ arduino/robot/src/sketch.ino
@@ -1,15 +1,18 @@
+
 #define CMD_READY 224
 #define CMD_SHUTDOWN 220 
 #define CMD_FORWARD 2
 #define CMD_BACKWARD 1
 #define CMD_STOP 0
+#define MC LOW
 const byte led = 13;
 const byte forward = 11;
 const byte backward = 10;
 const byte left = 6;
 const byte right = 7;
+const byte speaker = 3;
 boolean ledstate = 0;
-int code = 150;
+int code = 151;
 byte pwm = 0;
 
 
@@ -39,41 +42,33 @@ void loop() {
   //  Serial.println(code);
   }
   if(code == CMD_READY) {
-    digitalWrite(led, HIGH);
+    tone(speaker, 131, 1000);
+    code = 151;
   }
   if(code < 100 && code > 50) {
     digitalWrite(backward, LOW);
     byte pwm = map(code, 50, 100, 0, 225);
-    Serial.println(pwm);
+    //Serial.println(pwm);
     analogWrite(forward, pwm);
+    digitalWrite(led, HIGH);
   }
   if(code > 0 && code < 50) {
     digitalWrite(forward, LOW);
     pwm = map(code, 50, 0, 0, 225);
-    Serial.println(pwm); 
+    //Serial.println(pwm); 
     analogWrite(backward, pwm);
+    digitalWrite(led, HIGH);
   }
   if(code == 100) {digitalWrite(backward, LOW); digitalWrite(forward, HIGH);}
   if(code == 0) {digitalWrite(forward, LOW); digitalWrite(backward, HIGH);}
   if(code == 50) {
     digitalWrite(forward, LOW);
     digitalWrite(backward, LOW);
+    digitalWrite(led, LOW);
   }
   if(code == 151) {digitalWrite(left, LOW); digitalWrite(right, LOW);}
-  if(code < 151 && code >= 101) {digitalWrite(right, LOW); digitalWrite(left, HIGH); Serial.println("left");}
-  if(code <= 201 && code > 151) {digitalWrite(left, LOW); digitalWrite(right, HIGH); Serial.println("right");}
-//if(code == CMD_FORWARD) {
-// digitalWrite(backward, LOW);
-// digitalWrite(forward, HIGH); 
-//}
-//if(code == CMD_BACKWARD) {
-//digitalWrite(forward, LOW);
-//digitalWrite(backward, HIGH);
-//}
-//if(code == CMD_STOP) {
-//  digitalWrite(forward, LOW);
-//  digitalWrite(backward, LOW);
-//}
+  if(code < 151 && code >= 101) {digitalWrite(right, LOW); digitalWrite(left, HIGH); }
+  if(code <= 201 && code > 151) {digitalWrite(left, LOW); digitalWrite(right, HIGH); }
   delay(15);
 }
   
